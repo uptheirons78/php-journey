3258bbfb451bc8187e37316b78b9c395cac1bfdd
mauro bono <bono.mauro@gmail.com>
2021-05-03 21:36:12 +0200

    [add] more PDO features and error handlings

--- ProjectTwo/public/index.php
+++ ProjectTwo/public/index.php
@@ -1,9 +1,13 @@
 <?php
   try {
-    $pdo = new PDO('mysql:host=localhost;dbname:ijdb', 'Mauro', 'bnoRRT50');
+    $pdo = new PDO('mysql:host=localhost;dbname:ijdb;charset=utf8', 'Mauro', 'bnoRRT50');
+    $pdo->setAttribute(
+      PDO::ATTR_ERRMODE,
+      PDO::ERRMODE_EXCEPTION
+    );
     $output = 'Connected to the database.';
   } catch (PDOException $e) {
-    $output = 'Unable to connect to the database: ' . $e;
+    $output = 'Unable to connect to the database: ' . $e->getMessage() . ' in ' . $e->getFile() . ' line: ' . $e->getLine();
   }
   include __DIR__ . '/../templates/output.html.php';
 ?>
