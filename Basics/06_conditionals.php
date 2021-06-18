c7dae1846e936b88a9c9d190f7b0aba0543f2718
mauro bono <bono.mauro@gmail.com>
2021-06-18 22:28:17 +0200

    conditionals

--- Basics/06_conditionals.php
+++ Basics/06_conditionals.php
@@ -16,9 +16,30 @@ $salary = 300000;
 // if OR
 
 // Ternary if
-
+echo $age < 22 ? "young" : "old";
+echo "<br>";
 // Short ternary
+$actualAge = $age ?: 18;
+echo $actualAge;
+echo "<br>";
 
 // Null coalescing operator
-
+$myName = $name ?? "John";
+echo $myName;
+echo "<br>";
 // switch
+$userRole = 'editor';
+
+switch($userRole) {
+  case 'admin':
+    echo 'Admin';
+    break;
+  case 'editor':
+    echo 'Editor';
+    break;
+  case 'user':
+    echo 'User';
+    break;
+  default:
+    echo 'Invalid Role';
+}
