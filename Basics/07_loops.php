320fd0041b11fb7b09575de459d96ece1302bebb
mauro bono <bono.mauro@gmail.com>
2021-06-19 11:38:03 +0200

    loops

--- Basics/07_loops.php
+++ Basics/07_loops.php
@@ -1,13 +1,37 @@
 <?php
 
+$arr = array('one', 'two', 'three');
+$person = array(
+  'name' => 'Pippo',
+  'surname' => 'Pappo',
+  'position' => 'IT Consultant',
+);
+
 // while
-
-// Loop with $counter
-
+$i = 0;
+while($i < count($arr)) {
+  echo $arr[$i] . '<br>';
+  $i++;
+}
 // do - while
-
+echo '<br>' . '-----' . '<br>';
+$counter = 0;
+do {
+  echo $counter . '<br>';
+  $counter++;
+} while ($counter <= 10);
 // for
-
+echo '<br>' . '-----' . '<br>';
+for ($i = 0; $i < count($arr); $i++) {
+  echo $arr[$i] . '<br>';
+}
 // foreach
-
+echo '<br>' . '-----' . '<br>';
+foreach($arr as $element) {
+  echo $element . '<br>';
+}
 // Iterate Over associative array.
+echo '<br>' . '-----' . '<br>';
+foreach($arr as $key => $value) {
+  echo "$key: $value" . "<br>";
+}
