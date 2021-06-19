67819eba5396a4c1e0078a82855c78af210e0a3d
mauro bono <bono.mauro@gmail.com>
2021-06-19 12:56:28 +0200

    dates

--- Basics/09_dates.php
+++ Basics/09_dates.php
@@ -1,13 +1,21 @@
 <?php
-
+function lines() {
+  echo "<br> ------ <br>";
+}
 // Print current date
-
+echo date('Y-m-d H:i:s');
+lines();
 // Print yesterday
-
+echo date('Y-m-d H:i:s', time() - 60 * 60 * 24);
+lines();
 // Different format: https://www.php.net/manual/en/function.date.php
-
+echo date('j F Y, H:i:s');
+lines();
 // Print current timestamp
-
+echo time();
+lines();
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
+$parseDate = date_parse('2020-10-12 09:00:00');
+var_dump($parseDate);
+lines();
 
-// Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
