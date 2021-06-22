0d246976c5a2338c8a614a1210b3e2f4d83cd1d9
mauro bono <bono.mauro@gmail.com>
2021-06-22 21:04:50 +0200

    curl

--- Basics/13_curl/index.php
+++ Basics/13_curl/index.php
@@ -1,10 +1,35 @@
 <?php
 
+// curl_init()
+// curl_setopt()
+// curl_exec()
+// curl_close()
+
 $url = 'https://jsonplaceholder.typicode.com/users';
 // Sample example to get data.
+// $resource = curl_init($url);
+// curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+// $result = curl_exec($resource);
+// $info = curl_getinfo($resource, CURLINFO_HTTP_CODE, );
+// echo 'Response code: ' . $info . '<br>';
+// curl_close($resource);
+// echo $result;
 
-// Get response status code
+// POST REQUEST
+$resource = curl_init();
+$newUser = array(
+  'name' => 'Mimmo',
+  'surname' => 'Mammo',
+  'email' => 'hello@mimmomammo.com'
+);
+curl_setopt_array($resource, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_POST => true,
+  CURLOPT_HTTPHEADER => ['content-type: application/json'],
+  CURLOPT_POSTFIELDS => json_encode($newUser),
+));
 
-// set_opt_array
-
-// Post request
+$result = curl_exec($resource);
+curl_close($resource);
+echo $result;
