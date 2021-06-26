43b1f68482119d8ad5f2d1609b60e981feca6b8f
mauro bono <bono.mauro@gmail.com>
2021-06-26 10:08:21 +0200

    namespace, composer and autoloading

--- Basics/15_autoloading/index.php
+++ Basics/15_autoloading/index.php
@@ -1,1 +1,16 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+use app\Email as E;
+use app\Person;
+
+$email = new E();
+$person = new Person();
+
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+echo $response->getStatusCode(); // 200
+echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
